693ab05f069779cee23d4358f26f3db040f983f4
Anand <83801261+anand45151@users.noreply.github.com>
2023-09-24 12:06:46 +0530

    SEC

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import Layout from './components/Layout'
+import Sidebar from './components/Sidebar'
 import './App.scss'
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+    <Sidebar />
+      {/* <Routes> */}
+        {/* <Route path="/" element={<Layout />}> */}
+          {/* <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="Contact" element={<Contact />} />
-        
-        </Route>
-      </Routes>
+         */}
+        {/* </Route> */}
+      {/* </Routes> */}
     </>
   )
 }
